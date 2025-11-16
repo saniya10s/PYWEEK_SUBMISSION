b4b614eb41ad9230ad6d3a28acb446fd6fd4637e
saniya10s <sani305265@gmail.com>
2025-11-16 17:42:08 +0530

    Add files via upload

---
+++ Day_1.py
@@ -0,0 +1,20 @@
+# Data Types
+one = -2
+string="Hello"
+Switch=False
+
+print(string)
+print(Switch)
+print(one)
+input("Press Enter")
+
+name=input("Enter your name: ")
+print("Hello " + name)
+
+age = 14
+if age<0:
+    print("Invalid Age")
+elif age >= 18:
+    print("You are Eligible to vote.")
+else:
+    print("You are not Eligible to vote.")  
